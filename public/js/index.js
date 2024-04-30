b93940a5b93f374cfbededeae6a60ead50ce19d8
GuiSousa2910 <guilherme.agostinho@sptech.school>
2024-04-30 15:56:30 -0300

    home finalizada

---
+++ public/js/index.js
@@ -0,0 +1,9 @@
+function mostrar() {
+    document.getElementById('1').style.display = 'none';
+    document.getElementById('2').style.display = 'flex';
+}
+function desmostrar() {
+    document.getElementById('2').style.display = 'none';
+    document.getElementById('1').style.display = 'flex';
+    document.getElementById('1').style.animation = 'none';
+}
