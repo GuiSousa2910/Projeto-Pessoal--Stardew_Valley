0c943587e68e0e9409bc7409df5c4fac9d174eca
GuiSousa2910 <guilherme.agostinho@sptech.school>
2024-04-30 23:32:57 -0300

    iniciando tela sobre

--- public/js/index.js
+++ public/js/index.js
@@ -7,3 +7,23 @@ function desmostrar() {
     document.getElementById('1').style.display = 'flex';
     document.getElementById('1').style.animation = 'none';
 }
+
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.setinha-direita', {
+    duration: 1200,
+    origin: 'bottom'
+});
+
+sr.reveal('.txt-stardew', {
+    duration: 2200,
+    distace: '90px',
+    origin: 'left'
+});
+
+sr.reveal('.img-stardew', {
+    duration: 2700,
+    // delay: 500,
+    // distace: '90px',
+    origin: 'right'
+});
