e2a108bc9d9a22075d463e6bb6eb71b60e9b7feb
GuiSousa2910 <guilherme.agostinho@sptech.school>
2024-05-04 00:32:10 -0300

    finalizado home

--- public/js/index.js
+++ public/js/index.js
@@ -21,18 +21,161 @@ window.sr = ScrollReveal({ reset: true });
 
 sr.reveal('.setinha-direita', {
     duration: 2000,
+    delay: 300,
     origin: 'bottom',
     distance: '100px'
 });
 
 sr.reveal('.txt-stardew', {
     duration: 2500,
-    distance: '200px',
+    delay: 300,
+    distance: '100px',
     origin: 'left'
 });
 
 sr.reveal('.img-stardew', {
-    duration: 2700,
+    duration: 3000,
+    delay: 300,
     origin: 'right',
-    distance: '200px'
+    distance: '100px'
+});
+
+sr.reveal('.oque-fazer-titulo', {
+    duration: 2000,
+    delay: 300,
+    origin: 'bottom',
+    distance: '100px'
+});
+
+sr.reveal('.pesca-img', {
+    duration: 2000,
+    delay: 300,
+    origin: 'left',
+    distance: '100px'
+});
+
+sr.reveal('.setinha-esquerda', {
+    duration: 2000,
+    delay: 300,
+    origin: 'bottom',
+    distance: '100px'
+});
+
+sr.reveal('.pesca-texto-img', {
+    duration: 2000,
+    delay: 300,
+    origin: 'right',
+    distance: '100px'
+});
+
+sr.reveal('.img-amizade', {
+    duration: 2000,
+    delay: 300,
+    origin: 'bottom',
+    distance: '100px'
+});
+
+sr.reveal('.seta-direita', {
+    duration: 2000,
+    delay: 300,
+    origin: 'top',
+    distance: '100px'
+});
+
+sr.reveal('.seta-esquerda', {
+    duration: 2000,
+    delay: 300,
+    origin: 'bottom',
+    distance: '100px'
+});
+
+sr.reveal('.amizade-textos', {
+    duration: 2000,
+    delay: 300,
+    origin: 'top',
+    distance: '100px'
+});
+
+sr.reveal('.plantacao-textos', {
+    duration: 2000,
+    delay: 300,
+    origin: 'bottom',
+    distance: '100px'
+});
+
+sr.reveal('.img-plantacao', {
+    duration: 2000,
+    delay: 300,
+    origin: 'top',
+    distance: '100px'
+});
+
+sr.reveal('.animais-textos', {
+    duration: 2000,
+    delay: 300,
+    origin: 'left',
+    distance: '100px'
+});
+
+sr.reveal('.animais-img', {
+    duration: 2000,
+    delay: 300,
+    origin: 'right',
+    distance: '100px'
+});
+
+sr.reveal('.mineracao-textos', {
+    duration: 2500,
+    delay: 300,
+    origin: 'bottom',
+    distance: '130px'
+});
+
+sr.reveal('.mineracao-img', {
+    duration: 2000,
+    delay: 300,
+    origin: 'top',
+    distance: '100px'
+});
+
+sr.reveal('.explorar-textos', {
+    duration: 2000,
+    delay: 300,
+    origin: 'top',
+    distance: '100px'
+});
+
+sr.reveal('.img-explorar', {
+    duration: 2000,
+    delay: 300,
+    origin: 'bottom',
+    distance: '100px'
+});
+
+sr.reveal('.oque-fazer-titulo', {
+    duration: 2000,
+    delay: 300,
+    origin: 'bottom',
+    distance: '100px'
+});
+
+sr.reveal('.oque-fazer-titulo', {
+    duration: 2000,
+    delay: 300,
+    origin: 'bottom',
+    distance: '100px'
+});
+
+sr.reveal('.desenvolvedor-img', {
+    duration: 2000,
+    delay: 300,
+    origin: 'bottom',
+    distance: '100px'
+});
+
+sr.reveal('.desenvolvedor-textos', {
+    duration: 2000,
+    delay: 300,
+    origin: 'right',
+    distance: '100px'
 });
