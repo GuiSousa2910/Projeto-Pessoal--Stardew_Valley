bd891378baee533e3070cc1370dd186a99ecaec0
GuiSousa2910 <guilherme.agostinho@sptech.school>
2024-05-08 16:03:14 -0300

    melhorando

---
+++ public/js/animacaoFoto.js
@@ -0,0 +1,13 @@
+function mostrar() {
+    document.getElementById('1').style.display = 'none';
+    document.getElementById('2').style.display = 'flex';
+}
+function desmostrar() {
+    document.getElementById('2').style.display = 'none';
+    document.getElementById('1').style.display = 'flex';
+    document.getElementById('1').style.animation = 'none';
+}
+
+function descer() {
+    document.documentElement.style.scrollBehavior = 'smooth';
+}
--- public/js/scrollreveal.js
+++ public/js/scrollreveal.js
@@ -1,176 +1,140 @@
-function mostrar() {
-    document.getElementById('1').style.display = 'none';
-    document.getElementById('2').style.display = 'flex';
-}
-function desmostrar() {
-    document.getElementById('2').style.display = 'none';
-    document.getElementById('1').style.display = 'flex';
-    document.getElementById('1').style.animation = 'none';
-}
-
-function descer() {
-    document.documentElement.style.scrollBehavior = 'smooth';
-}
 
 window.sr = ScrollReveal({ reset: true });
 
 sr.reveal('.setinha-direita', {
     duration: 2000,
-    delay: 300,
     origin: 'bottom',
     distance: '100px'
 });
 
 sr.reveal('.txt-stardew', {
     duration: 2500,
-    delay: 300,
     distance: '100px',
     origin: 'left'
 });
 
 sr.reveal('.img-stardew', {
     duration: 3000,
-    delay: 300,
     origin: 'right',
     distance: '100px'
 });
 
 sr.reveal('.oque-fazer-titulo', {
     duration: 2000,
-    delay: 300,
     origin: 'bottom',
     distance: '100px'
 });
 
 sr.reveal('.pesca-img', {
     duration: 2000,
-    delay: 300,
     origin: 'left',
     distance: '100px'
 });
 
 sr.reveal('.setinha-esquerda', {
     duration: 2000,
-    delay: 300,
     origin: 'bottom',
     distance: '100px'
 });
 
 sr.reveal('.pesca-texto-img', {
     duration: 2000,
-    delay: 300,
     origin: 'right',
     distance: '100px'
 });
 
 sr.reveal('.img-amizade', {
     duration: 2000,
-    delay: 300,
-    origin: 'bottom',
+    origin: 'right',
     distance: '100px'
 });
 
 sr.reveal('.seta-direita', {
     duration: 2000,
-    delay: 300,
     origin: 'top',
     distance: '100px'
 });
 
 sr.reveal('.seta-esquerda', {
     duration: 2000,
-    delay: 300,
     origin: 'bottom',
     distance: '100px'
 });
 
 sr.reveal('.amizade-textos', {
     duration: 2000,
-    delay: 300,
-    origin: 'top',
+    origin: 'left',
     distance: '100px'
 });
 
 sr.reveal('.plantacao-textos', {
     duration: 2000,
-    delay: 300,
-    origin: 'bottom',
+    origin: 'right',
     distance: '100px'
 });
 
 sr.reveal('.img-plantacao', {
     duration: 2000,
-    delay: 300,
-    origin: 'top',
+    origin: 'left',
     distance: '100px'
 });
 
 sr.reveal('.animais-textos', {
     duration: 2000,
-    delay: 300,
     origin: 'left',
     distance: '100px'
 });
 
 sr.reveal('.animais-img', {
     duration: 2000,
-    delay: 300,
     origin: 'right',
     distance: '100px'
 });
 
 sr.reveal('.mineracao-textos', {
     duration: 2500,
-    delay: 300,
-    origin: 'bottom',
+    origin: 'right',
     distance: '130px'
 });
 
 sr.reveal('.mineracao-img', {
     duration: 2000,
-    delay: 300,
-    origin: 'top',
+    origin: 'left',
     distance: '100px'
 });
 
 sr.reveal('.explorar-textos', {
     duration: 2000,
-    delay: 300,
-    origin: 'top',
+    origin: 'left',
     distance: '100px'
 });
 
 sr.reveal('.img-explorar', {
     duration: 2000,
-    delay: 300,
+    origin: 'right',
+    distance: '100px'
+});
+
+sr.reveal('.oque-fazer-titulo', {
+    duration: 2000,
     origin: 'bottom',
     distance: '100px'
 });
 
 sr.reveal('.oque-fazer-titulo', {
     duration: 2000,
-    delay: 300,
-    origin: 'bottom',
-    distance: '100px'
-});
-
-sr.reveal('.oque-fazer-titulo', {
-    duration: 2000,
-    delay: 300,
     origin: 'bottom',
     distance: '100px'
 });
 
 sr.reveal('.desenvolvedor-img', {
     duration: 2000,
-    delay: 300,
-    origin: 'bottom',
+    origin: 'left',
     distance: '100px'
 });
 
 sr.reveal('.desenvolvedor-textos', {
     duration: 2000,
-    delay: 300,
     origin: 'right',
     distance: '100px'
 });
