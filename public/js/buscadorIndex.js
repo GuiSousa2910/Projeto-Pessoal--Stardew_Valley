35b4fe79408ee3b1be89a4f86c03b96a6914284c
GuiSousa2910 <guilherme.agostinho@sptech.school>
2024-05-17 00:00:32 -0300

    nav bar lateral

---
+++ public/js/buscadorIndex.js
@@ -0,0 +1,18 @@
+function jogar() {
+    window.location = 'whackmole.html';
+}
+
+function ia() {
+    window.location.href = 'http://localhost:3000/ia.html';
+}
+
+function buscar() {
+    window.location = 'pesquisa-personagem.html';
+}
+
+function voltar() {
+    window.location = 'index.html';
+}
+function ir() {
+    window.location = 'dashboard.html';
+}
