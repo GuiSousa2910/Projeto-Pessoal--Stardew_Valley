77dca659c02b3ffcc03093ab05dd35e3df6e1d45
GuiSousa2910 <guilherme.agostinho@sptech.school>
2024-05-24 14:38:43 -0300

    tentando inserir dados assim que ele se cadastra

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -1,4 +1,5 @@
 var database = require("../database/config");
+// var jogoCC = require("../controllers/jogo2Controller")
 
 function autenticar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha);
@@ -16,8 +17,9 @@ function cadastrar(nome, email, senha, nomeFazenda) {
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucaoSql = `
-    INSERT INTO fazendeiro (nome, email, senha, nomeFazenda) VALUES ('${nome}', '${email}', '${senha}', '${nomeFazenda}');
-    `;
+    INSERT INTO fazendeiro (nome, email, senha, nomeFazenda) VALUES ('${nome}', '${email}', '${senha}', '${nomeFazenda}');`;
+    // INSERT INTO jogoCC (idJogoCC, fkFazendeiro, pontosCC, qntVerde, qntAmarelo, qntRosa, qntRoxo, qntBranco, qntLaranja, hora) VALUES ('${jogoCC.id}','${jogoCC.idFazendeiro}','0', '0', '0', '0', '0', '0', '0', DATE_FORMAT(CURDATE(), '%d-%m'));
+    
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
